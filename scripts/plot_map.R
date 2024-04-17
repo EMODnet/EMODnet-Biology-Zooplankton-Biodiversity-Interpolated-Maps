b6f6a9f92e4e689d6c2069c61d39d3d6ed4a0c95
Torstensson Anders <a002440@ad.smhi.se>
2024-04-17 21:16:09 +0200

    added syke data

--- scripts/plot_map.R
+++ scripts/plot_map.R
@@ -3,6 +3,9 @@ library(finch)
 library(vegan)
 library(ggOceanMaps)
 library(sf)
+library(ggspatial)
+library(sp)
+library(geosphere)
 
 
 shapefilesDir <- "data/raw_data/shapefiles/sharkweb_shapefiles/"
@@ -15,6 +18,52 @@ basin_names <- read_delim(file.path(shapefilesDir, basin_names),
                           delim = ";", 
                           col_names = TRUE, 
                           locale = locale(encoding = "UTF-8"))
+
+
+
+syke_dwca <- dwca_read("data/raw_data/dwca-syke-zplank-v1.0.zip")
+
+# event_syke <- read_tsv(syke_dwca$files$txt_files[1])
+# occurrence_syke <- read_tsv(syke_dwca$files$txt_files[3])
+# emof_syke <- read_tsv(syke_dwca$files$txt_files[2])
+
+# event_syke <- read_tsv("data/raw_data/d6a04582-1fd2-4443-82ab-95fbccd3df74/")
+occurrence_syke <- read_csv("data/raw_data/d6a04582-1fd2-4443-82ab-95fbccd3df74/Occurrence.csv")
+emof_syke <- read_csv("data/raw_data/d6a04582-1fd2-4443-82ab-95fbccd3df74/MeasurementOrFact.csv")
+
+all_syke <- occurrence_syke %>%
+  left_join(emof_syke) %>%
+  filter(measurementtype == "Abundance of biological entity specified elsewhere per unit volume of the water body")
+
+coords <- all_syke %>%
+  select(eventid, eventdate, decimallatitude, decimallongitude)
+
+syke_diversity <- all_syke %>%
+  group_by(eventid) %>%
+  summarise(uniqueTaxa = length(unique(scientificname)),
+            shannon = diversity(as.numeric(measurementvalue))) %>%
+  left_join(coords) %>%
+  rename(id = eventid,
+         decimalLatitude = decimallatitude,
+         decimalLongitude = decimallongitude,
+         eventDate = eventdate) %>%
+  mutate(origin = "SYKE")
+
+# 
+# 
+# event_sel_syke <- event_syke %>%
+#   select(id, eventID, parentEventID, eventDate, minimumDepthInMeters, maximumDepthInMeters, decimalLatitude, decimalLongitude)
+# 
+# emof_sel_syke <- emof_syke %>%
+#   select(id, occurrenceID, measurementType, measurementValue, measurementUnit)
+# 
+# occurrence_sel_syke <- occurrence_syke %>%
+#   select(id, occurrenceID, measurementType, measurementValue, measurementUnit)
+# 
+# all_syke <- emof_syke %>%
+#   left_join(event_syke) %>%
+#   filter(!is.na(decimalLatitude))
+
 
 
 
@@ -49,17 +98,17 @@ baltic_sea_map <- basemap(
   bathymetry = FALSE
 )
 
-# Plot points on map
-map <- baltic_sea_map +
-  geom_spatial_point(
-    data = abundance,
-    aes(x = decimalLongitude, y = decimalLatitude, size = sample_sum),
-    pch = 21,
-    # size = 2,
-    fill = "red",
-    colour = "black"
-  ) +
-  scale_size_continuous(range = c(1, 10), name = "Abundance (ind/m3)")
+# # Plot points on map
+# map <- baltic_sea_map +
+#   geom_spatial_point(
+#     data = abundance,
+#     aes(x = decimalLongitude, y = decimalLatitude, size = sample_sum),
+#     pch = 21,
+#     # size = 2,
+#     fill = "red",
+#     colour = "black"
+#   ) +
+#   scale_size_continuous(range = c(1, 10), name = "Abundance (ind/m3)")
 
 
 # Calculate alpha diversity
@@ -69,6 +118,13 @@ data_diversity <- abundance %>%
             shannon = diversity(as.numeric(measurementValue))) %>%
   left_join(event)
 
+save(data_diversity, file= "data/derived_data/data_diversity.rda")
+
+diversity_all <- data_diversity %>%
+  select(id, eventDate, uniqueTaxa, shannon, decimalLatitude, decimalLongitude) %>%
+  mutate(origin = "SMHI") %>%
+  rbind(syke_diversity)
+
 # Calculate mean
 mean_diversity <- data_diversity %>%
   group_by(verbatimLocality) %>%
@@ -76,6 +132,56 @@ mean_diversity <- data_diversity %>%
             uniqueTaxa = mean(uniqueTaxa, na.rm = TRUE),
             decimalLongitude = mean(decimalLongitude, na.rm = TRUE),
             decimalLatitude = mean(decimalLatitude, na.rm = TRUE))
+
+
+
+proWG = CRS("+proj=longlat +datum=WGS84")
+
+cooridinates = diversity_all %>%
+  dplyr::select(decimalLongitude, decimalLatitude) %>%
+  distinct()
+
+xy = SpatialPointsDataFrame(
+  matrix(c(cooridinates$decimalLongitude,
+           cooridinates$decimalLatitude), 
+         ncol = 2), 
+  data.frame(ID = seq(1:nrow(cooridinates))),
+  proj4string = proWG)
+
+mdist = distm(xy)
+
+hc = hclust(as.dist(mdist), method = "complete")
+
+# Define the maximum distance for clustering stations together
+
+d = 20000
+
+xy$clust = cutree(hc, h = d)
+
+cooridinates = cooridinates %>%
+  mutate("stationCluster" = xy$clust)
+
+diversity_all = diversity_all %>%
+  left_join(cooridinates, by = c("decimalLongitude", "decimalLatitude"))
+
+
+
+
+
+
+
+
+
+
+# Calculate mean
+mean_diversity <- diversity_all %>%
+  mutate(position = paste(decimalLatitude, decimalLongitude)) %>%
+  group_by(stationCluster) %>%
+  summarise(shannon = mean(shannon, na.rm = TRUE),
+            uniqueTaxa = mean(uniqueTaxa, na.rm = TRUE),
+            decimalLongitude = mean(decimalLongitude, na.rm = TRUE),
+            decimalLatitude = mean(decimalLatitude, na.rm = TRUE),
+            origin = unique(origin))
 
 
 # Set CRS of basin layer
@@ -91,7 +197,7 @@ all_basins <- basins %>%
 all_basins <- st_transform(all_basins, 4326)
 
 # Add geometry information to data
-data_diversity_coordinates <- data_diversity %>%
+data_diversity_coordinates <- diversity_all %>%
   mutate(lon = decimalLongitude,
          lat = decimalLatitude)
 
@@ -112,7 +218,7 @@ data_diversity_st <- data_diversity_st %>%
   select(-geometry, -BASIN_NR)
 
 # Join with basin names
-data_diversity <- data_diversity %>%
+diversity_all <- diversity_all %>%
   left_join(data_diversity_st)
 
 # Plot points on map
@@ -126,11 +232,11 @@ map <- baltic_sea_map +
     colour = "black"
   ) +
   scale_size_continuous(range = c(1, 10), name = "Species richness") +
-  ggtitle("Average Zooplankton richness 1979-2022")
+  ggtitle("Average Zooplankton richness 1965-2023")
 
 
 # Plot time series of abundance at each location
-time_series <- ggplot(data_diversity, aes(x = eventDate, y = shannon)) +
+time_series <- ggplot(diversity_all, aes(x = eventDate, y = shannon)) +
   geom_point(aes(color = factor(location_sea_basin_en))) +
   scale_color_discrete() +
   labs(x = "Date", y = "Shannon index", title = "Zooplankton diversity", color = "Sea basin") +
@@ -140,7 +246,7 @@ time_series <- ggplot(data_diversity, aes(x = eventDate, y = shannon)) +
 ggsave(
   plot = time_series,
   path = "output/",
-  filename = "times_series.png",
+  filename = "times_series_all.png",
   device = "png",
   dpi = 300,
   width = 7,
@@ -151,7 +257,7 @@ ggsave(
 ggsave(
   plot = map,
   path = "output/",
-  filename = "map.png",
+  filename = "map_all.png",
   device = "png",
   dpi = 300,
   width = 7,
